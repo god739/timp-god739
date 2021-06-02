7ce781f7e39b559584e9413a83139b82f5a3c995
god739 <85251606+god739@users.noreply.github.com>
2021-06-03 03:47:33 +0700

    Add files via upload

---
+++ 02_Summ.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double z, x, c;
+    (void)scanf( "%lf %lf", &z, &x);
+    c = z + x;
+    printf("%lf\n", c);
+    return 0;
+}
---
+++ 03_Pow.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double q, w;
+    (void)scanf("%lf %lf", &q, &w);
+    printf("%lf\n", pow(q, w));
+    return 0;
+}
